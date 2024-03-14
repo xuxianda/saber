cb614b85e6f874b6d71c0461ad6b6a8de67ecd21
xuxianda <xuxianda01@beyondsoft.com>
2024-03-14 17:21:47 +0800

    上报设备类型、设备名

--- src/net/Net.cpp
+++ src/net/Net.cpp
@@ -1,14 +1,16 @@
 ﻿#include "Net.h"
 #include "httplib.h"
-
+#include "../util/SysUtils.h"
 
 std::string Net::post(std::string data, std::string host, std::string path, std::string token, std::string deviceId) {
+    SysUtils sys = SysUtils();
     httplib::Client client(host.c_str());
     client.set_read_timeout(10, 0);
     httplib::Headers headers;
     headers.insert({"accessToken", token});
     headers.insert({"deviceId", deviceId});
-
+    headers.insert({"deviceName", sys.getDeviceName()});
+    headers.insert({"deviceType", sys.getLocalSystemType()});
     auto res = client.Post(path.c_str(), headers, data.c_str(), "application/text");
     if (res && res->status == 200) {
         printf("数据上报远程响应：\n%s\n", res->body.c_str());
